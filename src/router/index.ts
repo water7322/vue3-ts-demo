f5280298740dc262ceeaedf07f1d997e0dec1818
zhangyitao <>
2022-01-17 21:09:10 +0800

    代码初始化

---
+++ src/router/index.ts
@@ -0,0 +1,37 @@
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router';
+/* Layout */
+import Layout from '@/layout'
+
+const routes = [
+    // 面试页
+    {
+        path: '/interview',
+        component: Layout,
+        redirect: '/interview/list',
+        children: [
+            {
+                path: 'list',
+                component: () =>
+                    import(
+                        /* webpackChunkName: "page/main/module/meetcode/interview/group/entry" */ '@/views/interview/list/index.vue'
+                    ),
+                meta: { title: '面试', permission: 'interview', entry: 'interview/list/index' }
+            }
+        ]
+    },
+    {
+        path: '/:catchAll(.*)',
+        redirect: '/interview/list'
+    }
+];
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    // history: createWebHistory(),
+    routes
+});
+
+router.beforeEach(async (to, from, next) => {
+    next();
+});
+export default router;
